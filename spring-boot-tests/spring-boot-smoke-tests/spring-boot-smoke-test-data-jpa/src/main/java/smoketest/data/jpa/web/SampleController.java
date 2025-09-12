67ffb595d74c20d610030f0c229196d2300cf5c9
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2025-09-12 10:59:18 -0700

    Demo that OSIV changes the current observation
    Set spring.jpa.open-in-view=false to "fix" it.

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-data-jpa/src/main/java/smoketest/data/jpa/web/SampleController.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-data-jpa/src/main/java/smoketest/data/jpa/web/SampleController.java
@@ -16,11 +16,13 @@
 
 package smoketest.data.jpa.web;
 
-import smoketest.data.jpa.service.CityService;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import smoketest.data.jpa.domain.City;
+import smoketest.data.jpa.service.CityRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -28,13 +30,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SampleController {
 
 	@Autowired
-	private CityService cityService;
+	private CityRepository cityRepository;
+
+	@Autowired
+	private ObservationRegistry observationRegistry;
 
 	@GetMapping("/")
 	@ResponseBody
-	@Transactional(readOnly = true)
 	public String helloWorld() {
-		return this.cityService.getCity("Bath", "UK").getName();
+		City city = this.cityRepository.findByNameAndCountryAllIgnoringCase("Bath", "UK");
+		Observation observation = this.observationRegistry.getCurrentObservation();
+		System.out.println(observation.getContextView().getName());
+		return city.getName();
 	}
 
 }
